d8724826e655117b61a0625759500a7eaff1acd1
Adam Blacke <adamblacke@gmail.com>
2022-02-22 15:03:09 -0500

    Updated Version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["pandas==1.4.1"]
 
 setup(
     name="stockeasy",
-    version="0.0.5",
+    version="0.0.6",
     author="Adam Blacke",
     author_email="adamblacke@gmail.com",
     description="A package for a quick and dirty portfolio analysis.",
