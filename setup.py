361974ed6e95caa55b3b121872110882ce760db2
Adam Blacke <adamblacke@gmail.com>
2022-02-22 12:49:00 -0500

    Start Docker Setup & testPyPi Push

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["pandas==1.4.1"]
+
+setup(
+    name="stockeasy",
+    version="0.0.3",
+    author="Adam Blacke",
+    author_email="adamblacke@gmail.com",
+    description="A package for a quick and dirty portfolio analysis.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AdamBlacke/stockeasy",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
