1bc393aa28da96d0be42a5b41b7312d29b01b400
Adam Blacke <adamblacke@gmail.com>
2022-02-22 11:45:04 -0500

    Initial Structure Setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # stockeasy
 Quick and Easy Stock Portfolio Analysis - FOR ENTERTAINMENT PURPOSES ONLY!!
+
+```
+pip install -r requirements_dev.txt
+```
---
+++ src/stockeasy/stockeasy.py
@@ -0,0 +1,13 @@
+def analyzer(config : dict, data : dict, logger : object):
+    """
+    This function completes a review of a provide stock portfolio.
+    Args:
+        config (dict): Configurable Options
+        data (dict): Dictionary of named Pandas Dataframes
+        logger (object): Standard Python Logger 
+    Returns:
+        None
+    """
+
+    print(f"Hello World")
+    
