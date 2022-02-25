582407998f2d727a2119eca8955c80f82986b2d2
Adam Blacke <adamblacke@gmail.com>
2022-02-25 09:55:47 -0500

    added testing for default contract and initial tooling

--- tests/unit/test_analyzer.py
+++ tests/unit/test_analyzer.py
@@ -1,12 +1,56 @@
+from unittest import result
+import pytest
 import stockeasy
 import logging
+import pandas as pd
+
+
+df_stocklist = pd.DataFrame(['vtsax'], columns=['symbol'])
 
 
 def test_init():
     assert 1 == 1
 
 
-def test_analyzer_inputs():
-    assert stockeasy.analyzer({'setting': 1}, {'input': []}, logging.getLogger('test-logger')) == 0
-    assert stockeasy.analyzer('', {'input': []}, logging.getLogger('test-logger')) == 1
-    assert stockeasy.analyzer({'setting': 1}, '', logging.getLogger('test-logger')) == 1
+# Default Contract Checks
+def test_analyzer_data_typecheck():
+    # wrong data type passed
+    with pytest.raises(TypeError):
+        stockeasy.analyzer(data=df_stocklist)
+
+    # expected data type passed
+    results = stockeasy.analyzer(data={'input': df_stocklist})
+    assert isinstance(results.get('output'), pd.DataFrame)
+
+
+def test_analyzer_config_typecheck():
+    # wrong data type passed
+    with pytest.raises(TypeError):
+        stockeasy.analyzer(config='')
+
+    # expected data type passed
+    results = stockeasy.analyzer(config={'setting 1': 'Anything'})
+    assert results.get('output') is None
+
+
+def test_analyzer_logger_typecheck():
+    # wrong data type passed
+    with pytest.raises(TypeError):
+        stockeasy.analyzer(logger='')
+
+    # expected data type passed
+    results = stockeasy.analyzer(logger=logging.getLogger('log'))
+    assert results.get('output') is None
+
+
+def test_analyzer_results_typecheck():
+    # Verify only named dataframes are returned
+    results = stockeasy.analyzer(data={'input': df_stocklist})
+    for item in results:
+        assert isinstance(results.get(item), pd.DataFrame)
+
+
+def test_analyzer_passthrough():
+    # default state is to act as a testing passthrough
+    results = stockeasy.analyzer({'input': df_stocklist})
+    assert results.get('output').equals(df_stocklist)
